8af5a67f9e9912bd81cc04b7e5ff9d8d97e16e59
Samiksha <mailtosamiksha.s@gmail.com>
2022-11-25 12:59:30 +0000

    question database add

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+let quiz_DB = [
+
+    {
+        question: “Q1: Which is the smallest ocean in the world ? ”,
+        a: “Indian”,
+        b: “Pacific”,
+        c: “Atlantic”,
+        d: “Artic”
+        ans: “ans4”
+    },
+
+    {
+        question: “Q2: The Dead Sea is located between which two countries”,
+        a: “Jordan and Israel ",
+        b: “Jordan and Sudan”,
+        c: “Turkey and UAE”,
+        d: “UAE and Egypt”,
+        ans: “ans1”
+    },
+
+    {
+        question: “Q3: Which country is called the playground of Europe”,
+        a: “Spain”,
+        b: ”Portugal”,
+        c: “Netherlands”,
+        d: “Switzerland”,
+        ans: “ans4”
+    },
+
+    {
+        question: “Q4: In which country white elephant is found ? ”,
+        a: “Thailand”,
+        b: “India”,
+        c: “Malaysia”,
+        d: “Srilanka”,
+        ans: ”ans1”
+    }
+];
